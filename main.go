17680f110ca57d6de8375cc136b4d776e697dc05
Joe <joeyfahnestock@gmail.com>
2024-05-21 00:18:34 -0400

    feat(create): Create app and serve root

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
 
+func main() {
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Test")
+	})
+	log.Fatal(app.Listen(":3000"))
 }
